0456baee194cd56db6f5fe3e0f23f3917109296a
Florian Weidinger <CrasyHorse@gmx.net>
2022-12-06 20:03:53 +0000

    Set test reporter to XUnit

--- .mocharc.js
+++ .mocharc.js
@@ -4,4 +4,6 @@ module.exports = {
     recursive: true,
     require: ["ts-node/register", "source-map-support/register"],
     spec: "test/**/*.spec.ts",
+    reporter: "xunit",
+    reporterOptions: "output=mocha-results.xml"
 }
